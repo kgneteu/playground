ffb82fe0f3d31e50a929b524a848af0faaa04d91
kgneteu <77528950+kgneteu@users.noreply.github.com>
2022-06-11 02:26:43 +0200

    start playing

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,16 @@ import Popup from "reactjs-popup";
 import 'reactjs-popup/dist/index.css';
 
 function FlexPlay() {
-    return (<div className="container bg-green-200 mx-auto">
-            <div className="flex flex-wrap w-full bg-amber-600 items-start h-96 content-center justify-start">
+    return (
+        <div className="container bg-green-200 mx-auto p">
+            <div className="flex flex-wrap w-full bg-amber-600 items-start h-96 content-center justify-start overflow-hidden">
                 <div className="bg-blue-500 h-10 flex-1/2">1</div>
                 <div className="bg-blue-500 flex-1/2">2</div>
                 <div className="bg-blue-500 flex-1/2 self-end">3</div>
                 <div className="bg-blue-500 h-20 flex-1/2">4</div>
                 <Popup
-                    trigger={<div className="bg-blue-500 hover:bg-red-600 flex-1/2 cursor-pointer text-lg text-center animate-pulse">
+                    trigger={<div
+                        className="bg-blue-500 hover:bg-red-600 flex-1/2 cursor-pointer text-lg text-center transition duration-700 ease-in-out transform hover:scale-125 peer-hover:-skew-x-6">
                         Click me</div>} position={"center center"}>
                     <p className={"bg-amber-400 p-4"}>Hello Junior</p>
                 </Popup>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,17 @@
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {
-      flex:{
-        '1/2':"50%",
-        '1/3':"33.3333%",
-        '2/5':"40%",
-        'full': "100%"
-      }
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx}',
+        './components/**/*.{js,ts,jsx,tsx}',
+    ],
+    theme: {
+        extend: {
+            flex: {
+                '1/2': "50%",
+                '1/3': "33.3333%",
+                '2/5': "40%",
+                'full': "100%"
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
